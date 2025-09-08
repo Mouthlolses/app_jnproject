491f560ed271621e019d5556fc60f45b7d50ddba
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-09-08 17:01:25 -0300

    opt_strings

--- app/src/main/java/com/example/app_jnproject/ui/components/Card.kt
+++ app/src/main/java/com/example/app_jnproject/ui/components/Card.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RippleConfiguration
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
