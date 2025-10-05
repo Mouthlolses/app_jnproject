164c3ed8e449527a92063d84b902fb87fd7ffe8b
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-10-05 16:47:34 -0300

    opt_navBarUi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.caririfest.app_jnproject"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
