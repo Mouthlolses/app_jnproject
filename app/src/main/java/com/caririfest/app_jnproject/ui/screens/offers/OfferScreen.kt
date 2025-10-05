33f08e4210148b48c1aa12040ba992ce6d5b93ed
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-10-05 16:41:47 -0300

    opt_padding

--- app/src/main/java/com/caririfest/app_jnproject/ui/screens/offers/OfferScreen.kt
+++ app/src/main/java/com/caririfest/app_jnproject/ui/screens/offers/OfferScreen.kt
@@ -94,7 +94,7 @@ fun OfferScreen(
             modifier = Modifier
                 .statusBarsPadding()
                 .fillMaxSize(),
-            contentPadding = PaddingValues(8.dp),
+            contentPadding = PaddingValues(8.dp, end = 8.dp, top = 8.dp, bottom = 100.dp),
             verticalArrangement = Arrangement.spacedBy(24.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
