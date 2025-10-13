a701387a9e10a7bdea76a517fdd263f641f5e50c
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-10-13 11:15:48 -0300

    appVersion_5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.caririfest.app_jnproject"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
