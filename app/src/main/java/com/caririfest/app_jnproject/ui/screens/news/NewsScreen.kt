95a3fcd331f6feaddcf91686e2dd10d27ce5727d
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-10-22 23:39:43 -0300

    new_advance_banner

--- app/src/main/java/com/caririfest/app_jnproject/ui/screens/news/NewsScreen.kt
+++ app/src/main/java/com/caririfest/app_jnproject/ui/screens/news/NewsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -21,18 +22,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Place
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -50,6 +50,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -180,6 +181,7 @@ fun NewsScreenLayout(
                 LazyColumn(
                     modifier = Modifier
                         .fillMaxSize()
+                        .background(Color(0xFFEFEFEF))
                         .padding(innerPadding)
                         .navigationBarsPadding(),
                     contentPadding = PaddingValues(bottom = 100.dp)
@@ -199,36 +201,37 @@ fun NewsScreenLayout(
                                     .padding(start = 12.dp, top = 12.dp, bottom = 8.dp)
                             )
                         }
-                        HorizontalPager(
-                            state = pagerState,
+                        Column(
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .background(Color(0xFFE6E6E6))
-                        ) { page ->
-                            val event = filterEvents[page]
-                            Card(
+                                .background(Color.White)
+                        ) {
+                            HorizontalPager(
+                                state = pagerState,
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .clickable(
-                                        onClick = { navController.navigate("newsDetailsScreen/${event.id}") },
-                                        enabled = true
-                                    )
-                                    .padding(
-                                        start = 16.dp,
-                                        end = 16.dp,
-                                        top = 16.dp,
-                                        bottom = 16.dp
-                                    )
-                            ) {
-                                Column(
-                                    Modifier
+                                    .height(360.dp)
+                            ) { page ->
+                                val event = uiState.events[page]
+                                Card(
+                                    modifier = Modifier
                                         .fillMaxWidth()
+                                        .clickable(
+                                            onClick = { navController.navigate("newsDetailsScreen/${event.id}") },
+                                            enabled = true
+                                        )
+                                        .padding(
+                                            start = 16.dp,
+                                            end = 16.dp,
+                                            top = 16.dp,
+                                            bottom = 16.dp
+                                        )
                                 ) {
-                                    Box(
-                                        modifier = Modifier
+                                    Column(
+                                        Modifier
                                             .fillMaxWidth()
-                                            .height(200.dp),
-                                        contentAlignment = Alignment.Center
+                                            .background(Color.White)
+                                            .padding(bottom = 16.dp),
+                                        horizontalAlignment = Alignment.CenterHorizontally
                                     ) {
                                         AsyncImage(
                                             model = event.fields.img.stringValue,
@@ -239,36 +242,63 @@ fun NewsScreenLayout(
                                                 .clip(RoundedCornerShape(16.dp)),
                                             contentScale = ContentScale.Crop
                                         )
+                                        Spacer(modifier = Modifier.height(12.dp))
+                                        Text(
+                                            text = event.fields.title.stringValue,
+                                            fontFamily = robotoFamily,
+                                            fontWeight = FontWeight.Bold,
+                                            fontSize = 18.sp,
+                                            color = Color(0xFF333333),
+                                            textAlign = TextAlign.Center,
+                                            modifier = Modifier
+                                                .padding(horizontal = 16.dp)
+                                        )
+                                        Spacer(modifier = Modifier.height(4.dp))
+                                        Text(
+                                            text = event.fields.date.stringValue,
+                                            fontFamily = robotoFamily,
+                                            fontWeight = FontWeight.Medium,
+                                            fontSize = 14.sp,
+                                            color = Color(0xFF666666),
+                                            textAlign = TextAlign.Center,
+                                            modifier = Modifier.padding(horizontal = 16.dp)
+                                        )
+                                        Spacer(modifier = Modifier.height(2.dp))
+                                        Text(
+                                            text = event.fields.location.stringValue,
+                                            fontFamily = robotoFamily,
+                                            fontWeight = FontWeight.Normal,
+                                            fontSize = 14.sp,
+                                            color = Color(0xFF888888),
+                                            textAlign = TextAlign.Center,
+                                            modifier = Modifier
+                                        )
                                     }
                                 }
                             }
-                        }
-                    }
-                    item {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 16.dp, end = 16.dp, top = 16.dp)
-                        ) {
-                            OutlinedTextField(
-                                value = query,
-                                onValueChange = { query = it },
-                                enabled = true,
-                                singleLine = true,
-                                placeholder = { Text(text = "Explore") },
-                                shape = RoundedCornerShape(26.dp),
-                                leadingIcon = {
-                                    Icon(
-                                        Icons.Default.Search,
-                                        contentDescription = "search"
-                                    )
-                                },
+                            Row(
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically,
                                 modifier = Modifier
-                                    .fillMaxWidth()
-                            )
+                                    .align(Alignment.CenterHorizontally)
+                            ) {
+                                repeat(uiState.events.size) { index ->
+                                    val isSelected = pagerState.currentPage == index
+                                    Box(
+                                        modifier = Modifier
+                                            .padding(4.dp)
+                                            .size(if (isSelected) 12.dp else 8.dp)
+                                            .background(
+                                                if (isSelected) Color.Black else Color.Gray,
+                                                shape = CircleShape
+                                            )
+                                    )
+                                }
+                            }
+                            Spacer(modifier = Modifier.height(26.dp))
                         }
                     }
-                    items(filterEvents) { doc ->
+                    items(uiState.events) { doc ->
                         Card(
                             modifier = Modifier
                                 .fillMaxWidth()
