485de9b8aff1538f88d42e67fa4cb949be0b11f8
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-12-05 13:03:58 -0300

    new_screen_impl

--- admin/src/main/java/com/caririfest/admin/ui/screens/validation_screens/producer_auth_account/ProducerAuthScreen.kt
+++ admin/src/main/java/com/caririfest/admin/ui/screens/validation_screens/producer_auth_account/ProducerAuthScreen.kt
@@ -1,11 +1,170 @@
 package com.caririfest.admin.ui.screens.validation_screens.producer_auth_account
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.caririfest.admin.R
 
 @Preview(showBackground = true)
 @Composable
 fun ProducerAuthScreen() {
+
+    var authAdminEmail by remember { mutableStateOf("") }
+    var authAdminPassword by remember { mutableStateOf("") }
+
+    var showPassword by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.caririfestlogo1),
+            contentDescription = "logo_image",
+            modifier = Modifier.height(160.dp)
+        )
+        Spacer(modifier = Modifier.padding(vertical = 28.dp))
+        OutlinedTextField(
+            value = authAdminEmail,
+            onValueChange = { authAdminEmail = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            label = { Text(text = "E-mail") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "email"
+                )
+            },
+            shape = RoundedCornerShape(18.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = authAdminPassword,
+            onValueChange = { authAdminPassword = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            label = { Text(text = "Senha") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "email"
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = { showPassword = !showPassword }) {
+                    Icon(
+                        imageVector = if (showPassword) Icons.Default.Visibility
+                        else Icons.Default.VisibilityOff,
+                        contentDescription = "password"
+                    )
+                }
+            },
+            shape = RoundedCornerShape(18.dp),
+            visualTransformation = if (showPassword)
+                VisualTransformation.None
+            else
+                PasswordVisualTransformation()
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Info,
+                    contentDescription = null,
+                    tint = Color.Gray,
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    "Wrong Password"
+                )
+            }
+            Spacer(modifier = Modifier.width(38.dp))
+
+            TextButton(
+                onClick = {},
+            ) { Text("Esqueceu sua senha?") }
+        }
+        Spacer(modifier = Modifier.height(26.dp))
+
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 26.dp),
+            contentPadding = PaddingValues(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFF9800),
+                contentColor = Color.White
+            ),
+            elevation = ButtonDefaults.buttonElevation(
+                4.dp
+            )
+        ) {
+            Text("Entrar", fontSize = 18.sp)
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Não tem conta ainda?")
+            TextButton(
+                onClick = {}
+            ) { Text("Sign up") }
+        }
+    }
 
 }
 
